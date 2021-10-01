e441742a60776135629e1b3e00448e0dde49c85e
ERICKGZ87 <erickgz87@gmail.com>
2021-10-01 01:57:04 +0000

    mis excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#Btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = GeneratorExcuse();
+  });
+};
+
+let GeneratorExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoindice = Math.floor(Math.random() * who.length);
+  let actionindice = Math.floor(Math.random() * action.length);
+  let whatindice = Math.floor(Math.random() * what.length);
+  let whenindice = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoindice] +
+    " " +
+    action[actionindice] +
+    " " +
+    what[whatindice] +
+    " " +
+    when[whenindice]
+  );
 };
